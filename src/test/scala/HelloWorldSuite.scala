489dfc3654f5716141f5776d694e62805ace2ae7
pamelarussell <pamela.russell@gmail.com>
2016-06-13 12:43:57 -0600

    Simple practice test

--- src/test/scala/HelloWorldSuite.scala
+++ src/test/scala/HelloWorldSuite.scala
@@ -1,6 +1,12 @@
+import org.scalatest.FunSuite
+
 /**
   * Created by prussell on 6/13/16.
   */
-class HelloWorldSuite {
+class HelloWorldSuite extends FunSuite {
+
+  test("should pass") {
+    assert(true)
+  }
 
 }
